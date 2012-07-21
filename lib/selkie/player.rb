610ff90c889984d21f7d2949dc0c3bcc6744ab89
david-huber <david.huber@gmail.com>
2012-07-20 20:53:13 -0500

    starting on zone methods

--- lib/selkie/player.rb
+++ lib/selkie/player.rb
@@ -4,7 +4,20 @@ module Selkie
     def self.included(base)
       base.extend ClassMethods
     end
-    
+   
+    def zones
+      @zones ||= {}
+    end
+
+    def has_zone(key, zone=nil)
+      raise TypeError, 'key must be a Symbol or Class' unless (key.kind_of? Symbol) || (key.kind_of? Class)
+      if key.kind_of? Class
+        zones[key.to_s.to_sym] = key.new
+      else
+        zones[key] = zone
+      end
+    end
+
     module ClassMethods
       def takes_turns
         include TurnMethods
@@ -19,7 +32,7 @@ module Selkie
       end
 
       def order=(value)
-        raise TypeError, "order must be a Fixnum" unless value.kind_of? Fixnum
+        raise TypeError, 'order must be a Fixnum' unless value.kind_of? Fixnum
         @order = value
       end
     end
--- spec/lib/selkie/player_spec.rb
+++ spec/lib/selkie/player_spec.rb
@@ -1,10 +1,54 @@
 require 'spec_helper'
 require 'selkie/player'
 
+describe 'Player' do
+  before :all do
+    class TypicalPlayer
+      include Selkie::Player    
+    end
+  end
+  
+  context 'can have zone' do
+    before :each do
+      @player = TypicalPlayer.new
+      class NotAZoneYet
+      end
+    end
 
+    it 'responds to zones' do
+      @player.should respond_to :zones
+    end
+  
+    it 'can have a symbol zone' do
+      @player.has_zone :hand
+      @player.zones.should have_key :hand
+    end
 
-describe "Player" do
-  context "takes turns" do
+    it 'can have a class zone' do
+      @player.has_zone NotAZoneYet
+      @player.zones.should have_key :NotAZoneYet
+      @player.zones[:NotAZoneYet].should be_an_instance_of(NotAZoneYet)
+    end
+
+    it 'can have an instance zone' do
+      @player.has_zone :grave_yard, NotAZoneYet.new
+      @player.zones.should have_key :grave_yard
+      @player.zones[:grave_yard].should be_an_instance_of(NotAZoneYet)
+    end
+  
+    it 'raises an error when key is neither a symbol or class' do
+      expect do
+        @player.has_zone 1, NotAZoneYet.new
+      end.to raise_error(TypeError, "key must be a Symbol or Class")
+      expect do
+        @player.has_zone [], NotAZoneYet.new
+      end.to raise_error(TypeError, "key must be a Symbol or Class")
+    end
+    
+  
+  end
+  
+  context 'takes turns' do
     before :all do
       class TurnTaker
         include Selkie::Player
